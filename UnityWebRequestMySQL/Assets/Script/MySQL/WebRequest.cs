4f7c76413d80c4c69368428d2767f67818286c42
Ebrahim Erik <ovierevaldi@gmail.com>
2019-07-20 15:55:22 +0700

    Addin WebRequest Script to Test Connection with Database

---
+++ UnityWebRequestMySQL/Assets/Script/MySQL/WebRequest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebRequest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetText());      
+    }
+
+    IEnumerator GetText()
+    {
+        using(UnityWebRequest www = 
+            UnityWebRequest.Get("https://shiplightsstudio.000webhostapp.com/UnityWebServiceMySQL/HelloWorld.php"))
+        {
+            yield return www.Send();
+
+            if(www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                /* Show Result as Text */
+                Debug.Log(www.downloadHandler.text);
+
+                /* Or retrieve results as binary data*/
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+}
